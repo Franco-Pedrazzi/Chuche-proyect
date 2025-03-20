127daba5bacf2e3ea3884024d27c4ebc5c8327a1
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2025-03-20 00:57:38 -0300

    primera forma de daño y de implementacion de vida

--- chuche-proyect--/evilNPC.gd
+++ chuche-proyect--/evilNPC.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
+@export var type="daño"
 var jugador=null
+var jugadorAux=null
 var SPEED=2.5
+@onready var animations=$AnimationPlayer
 var move=Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	if jugador!=null:
@@ -10,10 +13,24 @@ func _physics_process(delta: float) -> void:
 	move=move.normalized()*SPEED
 	move=move_and_collide(move)
 	move_and_slide()
-
+	
 
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body!=self:
 		jugador=body
+		jugadorAux=body
+		animations.play("caminar")
+
+
+func _on_area_2d_2_body_entered(body):
+	animations.play("pegar")
+	velocity.y = move_toward(velocity.y, 0, SPEED)
+	velocity.x = move_toward(velocity.x, 0, SPEED)
+	jugador=null
+	await get_tree().create_timer(0.2).timeout
+	animations.play("default")
+	await get_tree().create_timer(0.05).timeout
+	jugador=jugadorAux
+	
--- chuche-proyect--/objetos/personajes/codigo/mateo.gd
+++ chuche-proyect--/objetos/personajes/codigo/mateo.gd
@@ -1,19 +1,33 @@
 extends CharacterBody2D
-
+@export var lives=3
 var SPEED=300
-
+var notStop=true
 func _physics_process(delta):
 
 	var directionX := Input.get_axis("left", "right")
-	if directionX:
+	if directionX and notStop:
 		velocity.x = directionX * SPEED
-	else:
+	elif notStop:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	var directionY := Input.get_axis("up", "down")
-	if directionY:
+	if directionY and notStop:
 		velocity.y = directionY* SPEED
-	else:
+	elif notStop:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 		
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	if "type" in body:
+		if body.type=="daño":
+			lives-=1
+			print(lives)
+		var direccion=body.position-position
+		direccion.x/=abs(direccion.x)
+		direccion.y/=abs(direccion.y)
+		velocity= Vector2i(direccion.x * 180*-1,direccion.y* 180*-1)
+		notStop=false
+		await get_tree().create_timer(0.25).timeout
+		notStop=true
