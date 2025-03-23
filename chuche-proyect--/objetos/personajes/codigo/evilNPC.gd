cc628e5551e2a3f3285c9fe37a6e27d794fffe11
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2025-03-23 11:12:11 -0300

    mejora en la vida,daño del npc a mele e inicio de la creacion del shooter

---
+++ chuche-proyect--/objetos/personajes/codigo/evilNPC.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+@export var type="daño"
+@export var action="terminada"
+var jugador=null
+var esperar=false
+var SPEED=2.5
+var unidos=false
+@onready var animations=$AnimationPlayer
+var move=Vector2.ZERO
+func _physics_process(delta: float) -> void:
+	if jugador!=null and esperar==false:
+		move=position.direction_to(jugador.position)
+	else:
+		move=Vector2.ZERO
+	move=move.normalized()*SPEED
+	move=move_and_collide(move)
+	move_and_slide()
+	
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body!=self and "type" in body:
+		if body.type=="jugador":
+			jugador=body
+			animations.play("caminar")
+
+
+func _on_area_2d_2_body_entered(body):
+	if body==jugador:
+		print("asas")
+		animations.play("pegar")
+		esperar=true
+		unidos=true
+		await get_tree().create_timer(0.2).timeout
+		if unidos:
+			_on_area_2d_2_body_entered(body)
+func pegar():
+	action="efectivo"
+	animations.play("default")
+	await get_tree().create_timer(0.2).timeout
+	action="terminada"
+	await get_tree().create_timer(0.75).timeout
+	esperar=false
+	
+func _on_interaction_area_body_exited(body: Node2D) -> void:
+	unidos=false
--- chuche-proyect--/objetos/personajes/codigo/mateo.gd
+++ chuche-proyect--/objetos/personajes/codigo/mateo.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 @export var lives=3
+@export var type="jugador"
+var stop=false
 var SPEED=300
 var notStop=true
+var unidos=false
 func _physics_process(delta):
 
 	var directionX := Input.get_axis("left", "right")
@@ -19,15 +22,30 @@ func _physics_process(delta):
 	move_and_slide()
 
 
-func _on_area_2d_body_entered(body):
+func _on_interaction_area_body_entered(body):
+	stop=false
+	unidos=true
 	if "type" in body:
 		if body.type=="daño":
-			lives-=1
-			print(lives)
-		var direccion=body.position-position
-		direccion.x/=abs(direccion.x)
-		direccion.y/=abs(direccion.y)
-		velocity= Vector2i(direccion.x * 180*-1,direccion.y* 180*-1)
-		notStop=false
-		await get_tree().create_timer(0.25).timeout
-		notStop=true
+			await get_tree().create_timer(0.27).timeout
+			print(body.action)
+			if body.action=="efectivo" and stop==false:
+				lives-=1
+				var direccion=body.position-position
+				direccion.x/=abs(direccion.x)
+				direccion.y/=abs(direccion.y)
+				velocity= Vector2i(direccion.x * 180*-1,direccion.y* 180*-1)
+				notStop=false
+				await get_tree().create_timer(0.25).timeout
+				notStop=true
+			if lives<=0:
+				get_tree().reload_current_scene()
+				return 
+	await get_tree().create_timer(0.25).timeout
+	if unidos:
+		_on_interaction_area_body_entered(body)
+
+
+func _on_interaction_area_body_exited(body: Node2D) -> void:
+	stop=true
+	unidos=false
